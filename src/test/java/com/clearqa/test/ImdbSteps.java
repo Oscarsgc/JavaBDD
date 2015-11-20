198a3a0c48ef685729a466e75c939dbc376da4c9
Oscar Gonzalez <oscar_sgc@hotmail.com>
2015-11-20 17:56:08 -0400

    Added Various Cases

--- src/test/java/com/clearqa/test/ImdbSteps.java
+++ src/test/java/com/clearqa/test/ImdbSteps.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import cucumber.api.PendingException;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -14,40 +15,46 @@ import cucumber.api.java.en.*;
 
 public class ImdbSteps {
 
-	final Logger logger = LoggerFactory.getLogger(ImdbSteps.class);
-	private String imdb_url;
-	private JSONObject json_response;
+    final Logger logger = LoggerFactory.getLogger(ImdbSteps.class);
+    private String imdb_url;
+    private JSONObject json_response;
 
     @Given("^I query movie by \"([^\"]*)\"$")
-	public void I_query_movie_by_title(String key) throws UnsupportedEncodingException {
-		imdb_url = "http://www.omdbapi.com/?t=" + URLEncoder.encode(key, "UTF-8") + "&y=&plot=short&r=json";
-		System.out.println("++++++++"+imdb_url+"++++++++");
-		logger.info("http query = " + imdb_url);
-	}
+    public void I_query_movie_by_title(String key) throws UnsupportedEncodingException {
+        imdb_url = "http://www.omdbapi.com/?t=" + URLEncoder.encode(key, "UTF-8") + "&y=&plot=short&r=json";
+        System.out.println("++++++++" + imdb_url + "++++++++");
+        logger.info("http query = " + imdb_url);
+    }
 
     @Given("^I get movie by \"([^\"]*)\"$")
     public void I_query_movie_by_id(String key) throws UnsupportedEncodingException {
         imdb_url = "http://www.omdbapi.com/?i=" + URLEncoder.encode(key, "UTF-8") + "&plot=short&r=json";
-        System.out.println("++++++++"+imdb_url+"++++++++");
+        System.out.println("++++++++" + imdb_url + "++++++++");
         logger.info("http query = " + imdb_url);
     }
 
+    @When("^I make the rest call$")
+    public void I_make_the_rest_call() throws IOException, JSONException {
+        json_response = JsonReader.readJsonFromUrl(imdb_url);
+        logger.info("Response = " + json_response.toString());
+    }
 
-	@When("^I make the rest call$")
-	public void I_make_the_rest_call() throws IOException, JSONException {
-		json_response = JsonReader.readJsonFromUrl(imdb_url);
-		logger.info("Response = " + json_response.toString());
-	}
+    @Then("^response should contain:$")
+    public void response_should_contain_JSON(String expected_json_str) throws JSONException {
+        logger.info("Comparing reponse with" + expected_json_str);
+        JSONObject expected_json = new JSONObject(expected_json_str);
+        JSONAssert.assertEquals(expected_json, json_response, JSONCompareMode.LENIENT);
+    }
 
-	@Then("^response should contain:$")
-	public void response_should_contain_JSON(String expected_json_str) throws JSONException {
-		logger.info("Comparing reponse with" + expected_json_str);
-		JSONObject expected_json = new JSONObject(expected_json_str);
-		JSONAssert.assertEquals(expected_json, json_response, JSONCompareMode.LENIENT);
-	}
-	
-	@Then("^response should contain \"(.+)\"")
-	public void response_should_contain(String expecgted_json_str) throws JSONException {
-		response_should_contain_JSON(expecgted_json_str);
-	}
+    @Then("^response should contain \"(.+)\"")
+    public void response_should_contain(String expected_json_str) throws JSONException {
+        response_should_contain_JSON(expected_json_str);
+    }
+
+    @Given("^I get movie by \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void I_get_movie_by_title_and_id(String title, String year) throws Throwable {
+        imdb_url = "http://www.omdbapi.com/?t=" + URLEncoder.encode(title, "UTF-8") + "&y=" + URLEncoder.encode(year, "UTF-8") + "&plot=short&r=json";
+        System.out.println("++++++++" + imdb_url + "++++++++");
+        logger.info("http query = " + imdb_url);
+    }
 }
--- src/test/resources/com/clearqa/test/imdb_rest.feature
+++ src/test/resources/com/clearqa/test/imdb_rest.feature
@@ -42,17 +42,46 @@ Feature: IMDB rest api gets
       """
 
   Scenario Outline: Get many movies by id
-    Given I query movie by "<id>"
+    Given I get movie by "<id>"
     When I make the rest call
     Then response should contain "<Genre>"
 
     Examples: No special characters in movie titles
 
-      | id        | Genre                                    |
+      | id        | Genre                          |
       | tt0154789 | {"Genre":"Adventure, Fantasy"} |
-      | tt1285016 | {"Genre":"Biography, Drama"}      |
+      | tt1285016 | {"Genre":"Biography, Drama"}   |
 
     Examples: Special characters in movie titles
-      | title  | Genre                                    |
-      | WALL�E | {"Genre":"Animation, Adventure, Family"} |
-      | 8�     | {"Genre": "Mystery, Sci-Fi, Thriller"}   |
+      | id        | Genre                                    |
+      | tt0910970 | {"Genre":"Animation, Adventure, Family"} |
+      | tt1745873 | {"Genre":"Crime"}                        |
+
+  Scenario: Get movie by title and year
+    Given I get movie by "The Lord of the Rings" and "1992"
+    When I make the rest call
+    Then response should contain:
+      """
+      {
+      "Title":"The Lord of the Rings: Vol. II",
+      "Genre":"Action",
+      "Year":"1992"
+      }
+      """
+
+  Scenario Outline: Get many movies by title and year
+    Given I get movie by "<title>" and "<year>"
+    When I make the rest call
+    Then response should contain "<genre>"
+
+    Examples: No special characters in movie titles
+
+      | title        | year | genre                          |
+      | Finding Nemo | 2003 | {"Genre":"Animation, Adventure, Comedy"} |
+      | Inception    | 2010 | {"Genre":"Action, Mystery, Sci-Fi"}      |
+
+    Examples: Special characters in movie titles
+
+      | title  | year | genre                                    |
+      | WALL�E | 2008 | {"Genre":"Animation, Adventure, Family"} |
+      | 8�     | 2013 | {"Genre":"Crime, Drama"}                        |
